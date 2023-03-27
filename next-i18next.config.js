6ef9a6764208da0c14dc8944d0b00cefbcb4e601
Gia Thinh Nguyen <thinh-nguyen@hotmail.com>
2023-03-26 23:01:14 -0400

    fix i18n rendering on ssr pages

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,11 @@
+const path = require("path");
+const localePath = path.resolve("./public/locales");
+
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr"],
   },
+  localePath,
+  reloadOnPrerender: process.env.NODE_ENV === "development",
 };
